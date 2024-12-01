c160ea3dc4964d0200c5283ccd60355ef79139a3
Adriano Ibanez <dricoibanez@gmail.com>
2024-12-01 18:39:46 -0300

    Inicio do projeto, fazendo a lista das moedas a queserão convertidas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("********************************************\n");
+        System.out.println("Seja bem-vindo ao Conversor de Moedas!");
+        System.out.println("********************************************\n");
+        System.out.println("Escolha a moeda a ser convertida: \n");
+        System.out.println("1 - Real brasileiro >> Peso argentino ");
+        System.out.println("2 - Peso argentino >> Real brasileiro");
+        System.out.println("3 - Real Brasileiro >> Boliviano");
+        System.out.println("4 - Boliviano >> Real Brasileiro");
+        System.out.println("5 - Real brasileiro >> Peso chileno");
+        System.out.println("6 - Peso chileno >> Real brasileiro");
+        System.out.println("7 - Real brasileiro >> Peso colombiano");
+        System.out.println("8 - Peso colombiano >> Real brasileiro");
+        System.out.println("9 - Real brasileiro >> Dólar americano");
+        System.out.println("0 - Dólar americano>> Real brasileiro");
     }
 }
