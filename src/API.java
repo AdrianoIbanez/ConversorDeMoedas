7935fdaec1301f7ed9fdc9fa5d56e4d0d030f337
Adriano Ibanez <dricoibanez@gmail.com>
2024-12-02 20:14:23 -0300

    Linkando o API.java com o Main.java; Fiz alguns ajustes no API.java para executar a API corretamente; proximo passo fazer um menu para conversão das moedas.

--- src/API.java
+++ src/API.java
@@ -1,16 +1,25 @@
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
 public class API {
-    public static void main(String[] args) {
+
+    private static final String chaveApi = "56ef87de6edd55e8b8b7e8f7";
+
+    public String chamarApi(String moedas) throws IOException, InterruptedException {
+        var endereco = "https://v6.exchangerate-api.com/v6/" + chaveApi + "/latest/" + moedas.toUpperCase();
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://v6.exchangerate-api.com/v6/56ef87de6edd55e8b8b7e8f7/latest/USD"))
+                .uri(URI.create(endereco))
                 .build();
+
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+        return response.body();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,39 @@
+import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        API api = new API();
 
-       //menu inicial
-        System.out.println("********************************************");
-        System.out.println("Seja bem-vindo ao Conversor de Moedas!");
-        System.out.println("********************************************\n");
-        System.out.println("Escolha a moeda a ser convertida: \n");
-        System.out.println("1 - Real brasileiro >> Peso argentino ");
-        System.out.println("2 - Peso argentino >> Real brasileiro");
-        System.out.println("3 - Real Brasileiro >> Boliviano");
-        System.out.println("4 - Boliviano >> Real Brasileiro");
-        System.out.println("5 - Real brasileiro >> Peso chileno");
-        System.out.println("6 - Peso chileno >> Real brasileiro");
-        System.out.println("7 - Real brasileiro >> Peso colombiano");
-        System.out.println("8 - Peso colombiano >> Real brasileiro");
-        System.out.println("9 - Real brasileiro >> Dólar americano");
-        System.out.println("0 - Dólar americano>> Real brasileiro");
+        System.out.println("Digite a moeda base: ");
+        String moedas = leitura.nextLine();
+
+        try {
+            String resposta = api.chamarApi(moedas);
+            System.out.println("Resposta da API:");
+            System.out.println(resposta);
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Ocorreu um erro ao chamar a API: " + e.getMessage());
+
+
+            //menu inicial
+          /*  System.out.println("********************************************");
+            System.out.println("Seja bem-vindo ao Conversor de Moedas!");
+            System.out.println("********************************************\n");
+            System.out.println("Escolha a moeda a ser convertida: \n");
+            System.out.println("1 - Real brasileiro >> Peso argentino ");
+            System.out.println("2 - Peso argentino >> Real brasileiro");
+            System.out.println("3 - Real Brasileiro >> Boliviano");
+            System.out.println("4 - Boliviano >> Real Brasileiro");
+            System.out.println("5 - Real brasileiro >> Peso chileno");
+            System.out.println("6 - Peso chileno >> Real brasileiro");
+            System.out.println("7 - Real brasileiro >> Peso colombiano");
+            System.out.println("8 - Peso colombiano >> Real brasileiro");
+            System.out.println("9 - Real brasileiro >> Dólar americano");
+            System.out.println("0 - Dólar americano>> Real brasileiro");*/
+        }
     }
 }
